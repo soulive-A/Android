0e809829f875ea9e7c21e044542eb601ea41e287
gitsuhyun <suhyun10201@naver.com>
2024-02-29 12:46:03 +0900

    [Feat] 기업정보 카드 초기틀 작성

--- lib/screens/commercial/CommercialScreen.dart
+++ lib/screens/commercial/CommercialScreen.dart
@@ -11,27 +11,53 @@ class _CommercialScreen extends State<CommercialScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      backgroundColor: Color.fromRGBO(239, 239, 239, 100),
+      appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_rounded),
-        onPressed: (){
-            //뒤로가기 동작
-             }
-        ),
+            icon: Icon(Icons.arrow_back_rounded),
+            onPressed: () {
+              //뒤로가기 동작
+            }),
         foregroundColor: Colors.black,
         backgroundColor: Color.fromRGBO(239, 239, 239, 100),
         title: const Text(
-            "광고 상품 추가",
-            style: TextStyle(
-                fontFamily: 'kristall',
-                fontSize: 16),
+          "광고 상품 추가",
+          style: TextStyle(fontFamily: 'kristall', fontSize: 16),
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('광고분석입니다'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 22),
+              Text(
+                  "기업 정보",
+                  style: TextStyle(
+                    fontFamily: 'krisall',
+                    fontSize: 16,
+                    color: Color.fromRGBO(83, 83, 83, 100),
+                  ),
+                ),
+              const SizedBox(height: 22),
+
+              Center(
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 3),
+                  ),
+
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-
 }
