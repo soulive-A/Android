5e56332e053d37f37bfa001e74b73db8f4705628
gitsuhyun <suhyun10201@naver.com>
2024-02-29 12:11:26 +0900

    [Feat] appBar 생성

--- lib/screens/commercial/CommercialScreen.dart
+++ lib/screens/commercial/CommercialScreen.dart
@@ -11,6 +11,23 @@ class _CommercialScreen extends State<CommercialScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar:AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_rounded),
+        onPressed: (){
+            //뒤로가기 동작
+             }
+        ),
+        foregroundColor: Colors.black,
+        backgroundColor: Color.fromRGBO(239, 239, 239, 100),
+        title: const Text(
+            "광고 상품 추가",
+            style: TextStyle(
+                fontFamily: 'kristall',
+                fontSize: 16),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Text('광고분석입니다'),
       ),
