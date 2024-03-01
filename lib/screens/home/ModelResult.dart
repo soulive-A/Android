e8b3007d0a7941759b41b45fc003cc3faf24bf4d
seunghee17 <victoria9981@swu.ac.kr>
2024-03-02 00:31:02 +0900

    [feat] 모델결과 화면 생성
    ref #2

---
+++ lib/screens/home/ModelResult.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ModelResultScreen extends StatefulWidget{
+  const ModelResultScreen({super.key});
+
+  @override
+  State<ModelResultScreen> createState() => _ModelResultScreen();
+}
+
+class _ModelResultScreen extends State<ModelResultScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
