f50c6a0f37845e9e929b2fdd018302fef0290c4a
seunghee17 <victoria9981@swu.ac.kr>
2024-03-02 00:19:34 +0900

    [Feat] 모델 분석 결과 화면 생성 & 화면 이동 구현 #2

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soulive/screens/home/ModelResult.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +9,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreen extends State<HomeScreen> {
+  String _next = '/home';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('홈입니다'),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => ModelResultScreen())
+            );
+          },
+          child: Text('모델분석결과화면 임시 이동버튼'),
+        )
       ),
     );
   }
