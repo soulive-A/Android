261ac8d9ff0b93be488ca7cf9e17fc8bda32d516
seunghee17 <victoria9981@swu.ac.kr>
2024-03-02 09:48:41 +0900

    [feat] 모델 결과화면 viewmodel 생성

--- lib/screens/model_result/ModelResult.dart
+++ lib/screens/model_result/ModelResult.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:soulive/design/ColorStyles.dart';
 import 'package:soulive/design/FontStyles.dart';
 import 'package:soulive/design/SoulliveIcon.dart';
+import 'package:soulive/viewModel/TabViewModel.dart';
 
 class ModelResultScreen extends StatefulWidget{
   const ModelResultScreen({super.key});
@@ -13,20 +16,31 @@ class ModelResultScreen extends StatefulWidget{
 class _ModelResultScreen extends State<ModelResultScreen>{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: SoulliveIcon.arrowLeft(),
-          onPressed: () {
-            //뒤로가기 로직 추가
-          },
+    return ChangeNotifierProvider(
+        create: (_) => TabViewModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.bg,
+          leading: IconButton(
+            icon: SoulliveIcon.arrowLeft(),
+            onPressed: () {
+              //뒤로가기 로직 추가
+            },
+          ),
+          title: Text('모델 분석',
+              style: FontStyles.appbarFont),
+          centerTitle: true,
         ),
-        title: Text('모델 분석',
-        style: FontStyles.appbarFont),
-        centerTitle: true,
-      ),
-      body: Container(
-
+        body: Container(
+          color: AppColors.bg,
+          child: Column(
+            children: [
+              Container(
+                margin: ,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/viewModel/TabViewModel.dart
@@ -0,0 +1,13 @@
+//모델분석 결과를 위한 viewmodel
+
+import 'package:flutter/cupertino.dart';
+
+class TabViewModel extends ChangeNotifier{
+  int _selectedTab = 0;
+  int get selectedTab => _selectedTab;
+
+  void selectTab(int index){
+    _selectedTab = index;
+    notifyListeners()
+  }
+}
