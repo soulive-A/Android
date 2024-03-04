aab52ca6d149d81871c6cadea3a19a8d78dde509
seunghee17 <victoria9981@swu.ac.kr>
2024-03-04 19:04:14 +0900

    [Design] 컬러 시스템 최종 수정완료

--- lib/design/ColorStyles.dart
+++ lib/design/ColorStyles.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const Color m1 = Color(0xFFB950EF);
-  static const Color m2 = Color(0xFFC797D8);
-  static const Color m3 = Color(0xFFC2ACD8);
+  //static const Color m2 = Color(0xFFC797D8);
+  static const Color m3 = Color(0xFFEDD5FB);
+  static const Color m4 = Color(0xFF854BA7);
   static const Color s1 = Color(0xFFCEE8FF);
   static const Color s2 = Color(0xFFEFECF4);
   static const Color s3 = Color(0xFFFFFFFF);
+  static const Color s4 = Color(0xFFFFC120);
   static const Color bg = Color(0xFFF5F6FA);
   static const Color g1 = Color(0xFF505050);
   static const Color g2 = Color(0xFF6D6D6D);
@@ -14,6 +16,7 @@ class AppColors{
   static const Color g4 = Color(0xFFB6B6B6);
   static const Color g5 = Color(0xFFD9D9D9);
   static const Color g6 = Color(0xFFF0F0F0);
+  static Color g7 = Color(0xFFF0F0F0).withOpacity(0.6);
   static const Color border = Color(0xFFC2C2C2);
   static const Color black = Color(0xFF000000);
 }
