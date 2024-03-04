21cb6ed6ce26feff228ad634563ad53d1af66e9d
gitsuhyun <suhyun10201@naver.com>
2024-03-04 12:19:05 +0900

    [Feat] 폰트 추가 및 광고 페이지 위젯화

--- lib/design/FontStyles.dart
+++ lib/design/FontStyles.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:soulive/design/ColorStyles.dart';
 
 class FontStyles{
   static const logoFont = TextStyle(
@@ -8,4 +9,49 @@ class FontStyles{
   static const appbarFont = TextStyle(
       fontSize: 18, fontWeight: FontWeight.w600, fontFamily: 'pretendard'
   );
+
+  static const questionFont = TextStyle(
+    fontSize: 13, fontWeight: FontWeight.bold, fontFamily: 'pretendard', color: AppColors.g2,
+  );
+
+  static const hintFont = TextStyle(
+    fontSize: 13, fontWeight: FontWeight.normal, fontFamily: 'pretendard', color: AppColors.g5,
+  );
+
+  //확정나면 바꿔야함
+  // static const head1 = TextStyle(
+  //     fontSize: 16, fontWeight: FontWeight.bold, fontFamily: 'pretendard',
+  // );
+  //
+  // static const head2 = TextStyle(
+  //     fontSize: 13, fontWeight: FontWeight.bold, fontFamily: 'pretendard',
+  // );
+  //
+  // static const head3 = TextStyle(
+  //     fontSize: 16, fontWeight: FontWeight.normal, fontFamily: 'pretendard', height: 1.25,
+  // );
+  //
+  // static const sub1 = TextStyle(
+  //   fontSize: 16, fontWeight: FontWeight.bold, fontFamily: 'pretendard',
+  // );
+  //
+  // static const sub2 = TextStyle(
+  //   fontSize: 10, fontWeight: FontWeight.bold, fontFamily: 'pretendard',
+  // );
+  //
+  // static const sub3 = TextStyle(
+  //   fontSize: 16, fontWeight: FontWeight.normal, fontFamily: 'pretendard',
+  // );
+  //
+  // static const sub4 = TextStyle(
+  //   fontSize: 14, fontWeight: FontWeight.normal, fontFamily: 'pretendard',
+  // );
+  //
+  // static const sub5 = TextStyle(
+  //   fontSize: 10, fontWeight: FontWeight.normal, fontFamily: 'pretendard',
+  // );
+  //
+  // static const sub6 = TextStyle(
+  //   fontSize: 13, fontWeight: FontWeight.normal, fontFamily: 'pretendard',
+  // );
 }
