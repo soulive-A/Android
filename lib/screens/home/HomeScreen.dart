46c567611bdf83a5d94d1390710b3fe407c9c775
gitsuhyun <suhyun10201@naver.com>
2024-03-06 03:18:02 +0900

    [Design] 앱바 로고 위치 조정 및 배경 삽입

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soulive/design/SoulliveIcon.dart';
 import 'package:soulive/screens/home/InputModelScreen.dart';
 
 import '../../design/ColorStyles.dart';
@@ -15,40 +16,51 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreen extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: AppBar(
-          leading: Image.asset(
-            'assets/images/ic_logo.png',
-            width: 24,
-            height: 26,
-          ),
-          title: Text('SOUL MODEL',
-              style: FontStyles.AppTitle1.copyWith(color: Colors.white)),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(
-                begin: AlignmentDirectional.topCenter,
-                end: Alignment.bottomCenter,
-                colors: <Color>[
-                  AppColors.m1,
-                  Color(0xFFDAC7E1),
-                ],
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/background_home.png'),)
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70),
+          child: AppBar(
+            leading: Container(
+              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+              child: Image.asset(
+                'assets/images/ic_logo.png',
+                width: 24,
+                height: 26,
+              ),
+            ),
+            title: Text('SOUL MODEL',
+                style: FontStyles.AppTitle1.copyWith(color: Colors.white)),
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: AlignmentDirectional.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    AppColors.m1,
+                    Color(0xFFDAC7E1),
+                  ],
+                ),
               ),
             ),
           ),
         ),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => InputModelScreen()));
+          },
+          child: Text('모델분석결과화면 임시 인풋 버튼'),
+        )),
       ),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => InputModelScreen()));
-        },
-        child: Text('모델분석결과화면 임시 인풋 버튼'),
-      )),
     );
   }
 
